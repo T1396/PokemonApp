1682cca516357eb4c946b3e5d3a198c8c67936dc
Phil - Win 11 <philipp-tiropoulos@web.de>
2024-04-28 02:11:41 +0200

    local url to fetch data from and webclientId hidden in local properties + updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,12 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.devtools.ksp")
-    id("com.apollographql.apollo3") version "3.8.2"
+    id("com.apollographql.apollo3") version "3.8.3"
     id("kotlin-android")
     id("kotlin-parcelize")
 }
@@ -20,6 +22,14 @@ android {
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        val keystoreFile = project.rootProject.file("apikeys.properties")
+        val properties = Properties()
+        properties.load(keystoreFile.inputStream())
+        val localGraphQLUrl = properties.getProperty("GRAPH_URL_LOCAL")
+        val webClientId = properties.getProperty("WEB_CLIENT_ID")
+
+        buildConfigField("String", "localGraphqlUrl", localGraphQLUrl)
+        buildConfigField("String", "webClientId", webClientId)
     }
 
     buildTypes {
@@ -40,6 +50,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -51,26 +62,25 @@ dependencies {
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    val retrofitVersion = "2.9.0"
+    val retrofitVersion = "2.11.0"
     val roomVersion = "2.6.1"
     implementation("com.ultramegasoft.radarchart:radar-chart:0.1.5")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    // Optional - Annotationen für Lifecycle-Methoden
     //noinspection LifecycleAnnotationProcessorWithJava8
     annotationProcessor ("androidx.lifecycle:lifecycle-compiler:2.7.0")
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("com.google.android.gms:play-services-auth:21.0.0")
-    implementation("androidx.credentials:credentials:1.2.1")
-    implementation("androidx.credentials:credentials-play-services-auth:1.2.1")
+    implementation("com.google.android.gms:play-services-auth:21.1.0")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -78,26 +88,26 @@ dependencies {
 
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-firestore:24.10.3")
+    implementation("com.google.firebase:firebase-firestore:24.11.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation ("com.firebaseui:firebase-ui-auth:8.0.2")
     // Also add the dependency for the Google Play services library and specify its version
     implementation ("com.faltenreich:skeletonlayout:5.0.0")
 
 
 
-    implementation("io.coil-kt:coil:2.5.0")
-    implementation("com.apollographql.apollo3:apollo-runtime:3.8.2")
-    implementation("com.apollographql.apollo3:apollo-http-cache:3.8.2")
+    implementation("io.coil-kt:coil:2.6.0")
+    implementation("com.apollographql.apollo3:apollo-runtime:3.8.3")
+    implementation("com.apollographql.apollo3:apollo-http-cache:3.8.3")
 
     //Retrofit und Moshi
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
-    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
-    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.11.0")
     //Interceptor
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
     //Room
     implementation("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,15 @@ buildscript {
         val navVersion = "2.7.7"
         classpath("com.google.gms:google-services:4.4.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
-        classpath("com.apollographql.apollo3:apollo-runtime:3.8.2")
+        classpath("com.apollographql.apollo3:apollo-runtime:3.8.3")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
 
 }
+
