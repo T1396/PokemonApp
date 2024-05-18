113a1cefe558c8664f0043c9299ed72f0c1579ce
johndoe <philipp-tiropoulos@web.de>
2024-05-19 00:37:44 +0200

    updated team builder (extracted dialogs) + changes in logic in teambuilder + made more functions for fragment

--- app/src/main/java/com/example/pokinfo/adapter/teamAndTeambuilder/SpinnerAdapter.kt
+++ app/src/main/java/com/example/pokinfo/adapter/teamAndTeambuilder/SpinnerAdapter.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.example.pokinfo.R
+import com.example.pokinfo.adapter.home.detail.AbilityEffectText
 
-class SpinnerAdapter(context: Context,
-                     resource: Int,
-                     objects: List<Pair<String, String>>)
-    : ArrayAdapter<Pair<String, String>>(context, resource, objects) {
+class SpinnerAdapter(
+    context: Context,
+    resource: Int,
+    abilityList: List<AbilityEffectText>
+): ArrayAdapter<AbilityEffectText>(context, resource, abilityList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         return createItemView(position, convertView, parent)
@@ -22,12 +24,12 @@ class SpinnerAdapter(context: Context,
     }
 
     private fun createItemView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val (name, effect) = getItem(position) ?: Pair("Error", "Error")
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.spinner_item, parent, false)
+        val ability = getItem(position) ?: return View(context)
         val nameTextView = view.findViewById<TextView>(R.id.nameTextView)
         val effectTextView = view.findViewById<TextView>(R.id.effectTextView)
-        nameTextView.text = name
-        effectTextView.text = effect
+        nameTextView.text = ability.name
+        effectTextView.text = ability.textShort
         return view
     }
 }
