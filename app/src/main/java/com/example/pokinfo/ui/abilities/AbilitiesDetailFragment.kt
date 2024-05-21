fb8833b70ad506401d59945f792035d2cea387e0
johndoe <philipp-tiropoulos@web.de>
2024-05-22 01:16:43 +0200

    used AbilitySheetAdapter instead of old design

--- app/src/main/java/com/example/pokinfo/ui/abilities/AbilitiesDetailFragment.kt
+++ app/src/main/java/com/example/pokinfo/ui/abilities/AbilitiesDetailFragment.kt
@@ -1,18 +1,18 @@
 package com.example.pokinfo.ui.abilities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.example.pokinfo.R
-import com.example.pokinfo.adapter.home.PokeListAdapter
 import com.example.pokinfo.adapter.home.detail.AbilityEffectText
+import com.example.pokinfo.adapter.home.detail.AbilitySheetAdapter
 import com.example.pokinfo.data.models.database.pokemon.PokemonTypeName
 import com.example.pokinfo.databinding.FragmentAbilitiesDetailBinding
-import com.example.pokinfo.ui.misc.dialogs.openPokemonListDialog
+import com.example.pokinfo.ui.misc.dialogs.showConfirmationDialog
 import com.example.pokinfo.viewModels.AbilityViewModel
 import com.example.pokinfo.viewModels.PokeViewModel
 import com.example.pokinfo.viewModels.SharedViewModel
@@ -28,7 +28,7 @@ class AbilitiesDetailFragment : Fragment() {
         ViewModelFactory(requireActivity().application, sharedViewModel)
     }
     private val abilityViewModel: AbilityViewModel by activityViewModels()
-    private lateinit var adapter: PokeListAdapter
+    private lateinit var adapter: AbilitySheetAdapter
     private var languageId = 9
 
 
@@ -57,43 +57,29 @@ class AbilitiesDetailFragment : Fragment() {
         }
 
         abilityViewModel.abilityEffectTexts.observe(viewLifecycleOwner) { abilityDetails ->
-            val ability = abilityDetails.firstOrNull()
+            val ability = abilityDetails.firstOrNull() ?: return@observe
             setupPokemonListRv(typeNames, ability)
-            ability?.let {
-                binding.tvAbilityName.text = it.name
-                binding.details.tvEffectLongText.text = it.textLong
-                binding.details.tvAbilityEffectText.text = it.textShort
-            }
         }
     }
 
     private fun setupPokemonListRv(
         typeNames: List<PokemonTypeName>,
-        ability: AbilityEffectText?
+        ability: AbilityEffectText
     ) {
         abilityViewModel.getPokemonListWhoHaveAbility { pokemonList ->
-            adapter = PokeListAdapter(
-                typeNames = typeNames,
-                onItemClicked = { id ->
-                    openPokemonListDialog(
-                        listOfPokemon = pokemonList,
-                        title = getString(R.string.every_pokemon_with_ability, ability?.name),
-                        typeNames = typeNames,
-                    ) { pokemonId ->
-                        // another dialog to ask user if he surely wants to navigate
-                        pokeViewModel.getSinglePokemonData(
-                            pokemonId,
-                            R.string.failed_load_single_pokemon_data
-                        )
-                        findNavController().navigate(
-                            AbilitiesDetailFragmentDirections.actionAbilitiesDetailFragmentToNavHomeDetail(
-                                pokemonId
-                            )
-                        )
+            adapter = AbilitySheetAdapter(ability, typeNames) { pokemonId ->
+                showConfirmationDialog(
+                    onConfirm = {
+                        pokeViewModel.getSinglePokemonData(pokemonId)
+                        findNavController().navigate(AbilitiesDetailFragmentDirections.actionAbilitiesDetailFragmentToNavHomeDetail(pokemonId))
                     }
-                }
-            )
-            adapter.submitList(pokemonList)
+                )
+            }
+            requireActivity().runOnUiThread {
+                binding.rvPokeList.adapter = adapter
+                adapter.submitList(pokemonList)
+            }
+
         }
     }
 }
