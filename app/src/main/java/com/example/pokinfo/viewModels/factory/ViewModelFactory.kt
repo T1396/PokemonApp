2d3e9d5dc7a9a856bc2e066a565034bf136a05e4
johndoe <philipp-tiropoulos@web.de>
2024-05-21 02:41:39 +0200

    added attacks/ability viewmodel for factory

--- app/src/main/java/com/example/pokinfo/viewModels/factory/ViewModelFactory.kt
+++ app/src/main/java/com/example/pokinfo/viewModels/factory/ViewModelFactory.kt
@@ -3,6 +3,8 @@ package com.example.pokinfo.viewModels.factory
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.pokinfo.viewModels.AbilityViewModel
+import com.example.pokinfo.viewModels.AttacksViewModel
 import com.example.pokinfo.viewModels.FirebaseViewModel
 import com.example.pokinfo.viewModels.PokeViewModel
 import com.example.pokinfo.viewModels.SharedViewModel
@@ -25,6 +27,14 @@ class ViewModelFactory(
             modelClass.isAssignableFrom(PokeViewModel::class.java) -> {
                 @Suppress("UNCHECKED_CAST")
                 PokeViewModel(application, sharedViewModel) as T
+            }
+            modelClass.isAssignableFrom(AttacksViewModel::class.java) -> {
+                @Suppress("UNCHECKED_CAST")
+                AttacksViewModel(application, sharedViewModel) as T
+            }
+            modelClass.isAssignableFrom(AbilityViewModel::class.java) -> {
+                @Suppress("UNCHECKED_CAST")
+                AbilityViewModel(application, sharedViewModel) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class")
         }
