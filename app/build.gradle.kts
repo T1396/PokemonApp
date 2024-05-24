69ddcf88ae201de5258d86218c1571b12c7a7ece
johndoe <philipp-tiropoulos@web.de>
2024-05-25 01:56:00 +0200

    dependencies updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
@@ -74,11 +74,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     //noinspection LifecycleAnnotationProcessorWithJava8
-    annotationProcessor ("androidx.lifecycle:lifecycle-compiler:2.7.0")
+    annotationProcessor ("androidx.lifecycle:lifecycle-compiler:2.8.0")
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
