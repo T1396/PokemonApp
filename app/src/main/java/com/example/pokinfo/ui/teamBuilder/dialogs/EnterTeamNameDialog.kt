1af32bd82478d4546ce102dacf0f0c7ace9bb956
johndoe <philipp-tiropoulos@web.de>
2024-05-30 22:31:20 +0200

    updated Dialog to edit an existing team name as well + resizing on smaller devides

--- app/src/main/java/com/example/pokinfo/ui/teamBuilder/dialogs/EnterTeamNameDialog.kt
+++ app/src/main/java/com/example/pokinfo/ui/teamBuilder/dialogs/EnterTeamNameDialog.kt
@@ -1,19 +1,31 @@
 package com.example.pokinfo.ui.teamBuilder.dialogs
 
-import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import android.widget.CheckBox
+import android.util.Log
+import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
 import com.example.pokinfo.R
+import com.example.pokinfo.databinding.PopupCreateTeamDialogBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textfield.TextInputLayout
 
 class EnterTeamNameDialogFragment : DialogFragment() {
     interface EnterTeamNameListener {
-        fun onTeamNameEntered(name: String, isPublic: Boolean)
+        fun onTeamNameEntered(name: String, isPublic: Boolean, teamId: String)
+    }
+
+    companion object {
+        fun newInstance(isPublic: Boolean, teamId: String, oldName: String): EnterTeamNameDialogFragment {
+            val args = Bundle()
+            args.putBoolean("isPublic", isPublic)
+            args.putString("teamId", teamId)
+            args.putString("oldName", oldName)
+            val fragment = EnterTeamNameDialogFragment()
+            fragment.arguments = args
+            return fragment
+        }
     }
 
     private var listener: EnterTeamNameListener? = null
@@ -23,34 +35,65 @@ class EnterTeamNameDialogFragment : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val view = layoutInflater.inflate(R.layout.popup_create_team_dialog, null)
-        val inputLayout = view.findViewById<TextInputLayout>(R.id.tilTeamName)
-        val checkBox = view.findViewById<CheckBox>(R.id.cbPublic)
+        val binding = PopupCreateTeamDialogBinding.inflate(layoutInflater)
+
+        // get values that are eventually set over companion object
+        val isPublicInitial = arguments?.getBoolean("isPublic", false) ?: false
+        binding.cbPublic.isChecked = isPublicInitial
+        val oldTeamName = arguments?.getString("oldName")
+        val teamId = arguments?.getString("teamId") ?: ""
+        if (oldTeamName != null) binding.tvTitle.text = getString(R.string.enter_new_teamname_title)
+
+
+        binding.tilTeamName.editText?.addTextChangedListener {
+            val isValidName = !it.isNullOrBlank() && it.toString() != oldTeamName
+            binding.tilTeamName.error = if (isValidName) null else if (oldTeamName != null) {
+                if (it.toString() == oldTeamName) {
+                    getString(R.string.need_new_name_error)
+                } else {
+                    getString(R.string.enter_name_error)
+                }
+            } else {
+                getString(R.string.enter_name_error)
+            }
+            binding.saveButton.isEnabled = isValidName
+        }
+        binding.tilTeamName.editText?.setText("")
+
 
         return MaterialAlertDialogBuilder(requireContext())
-            .setView(view)
-            .setNegativeButton(getString(R.string.cancel), null)
-            .setPositiveButton(getString(R.string.save), null) // Initially set to null
+            .setView(binding.root)
             .create().apply {
                 setOnShowListener {
-                    val posButton = getButton(AlertDialog.BUTTON_POSITIVE)
-                    posButton.isEnabled = false
+                    binding.saveButton.isEnabled = false
 
 
-                    inputLayout.editText?.addTextChangedListener {
-                        val isNotBlank = !it.isNullOrBlank()
-                        inputLayout.error = if (isNotBlank) null else getString(R.string.enter_name_error)
-                        posButton.isEnabled = isNotBlank
+                    binding.tilTeamName.editText?.setOnFocusChangeListener { v, hasFocus ->
+                        if (hasFocus) {
+                            resizeHeightOnSmallDevices(binding)
+                        }
                     }
-
-                    posButton.setOnClickListener {
-                        val teamName = inputLayout.editText?.text.toString().trim()
-                        val isPublic = checkBox.isChecked
-                        listener?.onTeamNameEntered(teamName, isPublic)
+                    binding.cancelButton.setOnClickListener {
+                        dismiss()
+                    }
+                    binding.saveButton.setOnClickListener {
+                        val teamName = binding.tilTeamName.editText?.text.toString().trim()
+                        val isPublic = binding.cbPublic.isChecked
+                        listener?.onTeamNameEntered(teamName, isPublic, teamId)
                         dismiss()
                     }
                 }
             }
+    }
+
+    private fun resizeHeightOnSmallDevices(binding: PopupCreateTeamDialogBinding) {
+        val displayMetrics = requireContext().resources.displayMetrics
+        val screenHeightDp = displayMetrics.heightPixels / displayMetrics.density
+        if (screenHeightDp < 700) {
+            binding.scrollView.smoothScrollTo(0, binding.root.bottom)
+            val maxHeight = (displayMetrics.heightPixels * 0.7).toInt()
+            dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, maxHeight)
+        }
     }
 
     override fun onDetach() {
