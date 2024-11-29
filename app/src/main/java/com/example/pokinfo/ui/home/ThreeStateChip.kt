dddcdddd882ae30b34c9724cab07e1b3413e69ce
Philipp T <ptirop@icloud.com>
2024-11-29 22:53:27 +0100

    enum renamed

--- app/src/main/java/com/example/pokinfo/ui/home/ThreeStateChip.kt
+++ app/src/main/java/com/example/pokinfo/ui/home/ThreeStateChip.kt
@@ -1,15 +1,12 @@
 package com.example.pokinfo.ui.home
 
 import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.util.AttributeSet
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import com.example.pokinfo.R
-import com.example.pokinfo.data.enums.PokemonSortFilterState
+import com.example.pokinfo.data.enums.PokemonSortOption
 import com.google.android.material.chip.Chip
-import com.google.android.material.color.MaterialColors
 
 class ThreeStateChip @JvmOverloads constructor(
     context: Context,
@@ -24,7 +21,7 @@ class ThreeStateChip @JvmOverloads constructor(
     private val ascIcon = AppCompatResources.getDrawable(context, R.drawable.baseline_arrow_drop_up_24)
     private val descIcon = AppCompatResources.getDrawable(context, R.drawable.baseline_arrow_drop_down_24)
 
-    var state: PokemonSortFilterState = PokemonSortFilterState.INACTIVE
+    var state: PokemonSortOption = PokemonSortOption.INACTIVE
         set(value) {
             field = value
             updateView()
@@ -32,19 +29,19 @@ class ThreeStateChip @JvmOverloads constructor(
 
     fun nextState() {
         state = if (!ascendingFirst) when(state) {
-            PokemonSortFilterState.INACTIVE -> PokemonSortFilterState.DESCENDING
-            PokemonSortFilterState.DESCENDING -> PokemonSortFilterState.ASCENDING
-            PokemonSortFilterState.ASCENDING -> PokemonSortFilterState.INACTIVE
+            PokemonSortOption.INACTIVE -> PokemonSortOption.DESCENDING
+            PokemonSortOption.DESCENDING -> PokemonSortOption.ASCENDING
+            PokemonSortOption.ASCENDING -> PokemonSortOption.INACTIVE
         } else when(state) {
-            PokemonSortFilterState.INACTIVE -> PokemonSortFilterState.ASCENDING
-            PokemonSortFilterState.ASCENDING -> PokemonSortFilterState.DESCENDING
-            PokemonSortFilterState.DESCENDING -> PokemonSortFilterState.INACTIVE
+            PokemonSortOption.INACTIVE -> PokemonSortOption.ASCENDING
+            PokemonSortOption.ASCENDING -> PokemonSortOption.DESCENDING
+            PokemonSortOption.DESCENDING -> PokemonSortOption.INACTIVE
 
         }
     }
 
     fun resetState() {
-        state = PokemonSortFilterState.INACTIVE
+        state = PokemonSortOption.INACTIVE
     }
 
     init {
@@ -58,15 +55,15 @@ class ThreeStateChip @JvmOverloads constructor(
     private fun updateView() {
 
         when (state) {
-            PokemonSortFilterState.INACTIVE -> {
+            PokemonSortOption.INACTIVE -> {
                 isSelected = false
                 chipIcon = null
             }
-            PokemonSortFilterState.ASCENDING -> {
+            PokemonSortOption.ASCENDING -> {
                 isSelected = true
                 chipIcon = ascIcon
             }
-            PokemonSortFilterState.DESCENDING -> {
+            PokemonSortOption.DESCENDING -> {
                 isSelected = true
                 chipIcon = descIcon
             }
